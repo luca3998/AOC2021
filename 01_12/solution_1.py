97988a925f8275ee83f767423cf3140798328986
Luca <luca@mbp-van-luca.home>
2021-12-01 11:36:48 +0100

    Implement both solutions for parts 1 and 2 of Day 1

--- 01_12/solution_1.py
+++ 01_12/solution_1.py
@@ -4,8 +4,19 @@ with open('input.txt') as f:
 
 count = 0;
 
+# Part 1
 for num,current in enumerate(numbers):
     if num == 0: pass
     if current > numbers[num-1]: count += 1
 
 print(count)
+
+count_3 = 0;
+# Part 2
+for num,current in enumerate(numbers):
+    if num == (len(numbers) -  3): break
+    one = current + numbers[num+1] + numbers[num+2]
+    two = numbers[num+1] + numbers[num+2] + numbers[num+3]
+    if two > one: count_3 += 1
+
+print(count_3)
